89db35abcb7a60a3894471f15e458bac6adb1bcc
Mehedi <mehedi33mail@gmail.com>
2015-12-03 22:58:41 +0600

    Ajax Request to Server

---
+++ cword-master/oy-cword-1.0/js/process.php
@@ -0,0 +1,11 @@
+<?php
+	$con = mysql_connect("localhost","root","");
+	$db = mysql_select_db("demo", $con);
+
+	$text = $_POST['text'];
+	
+	$query="INSERT INTO banglameaning  VALUES(1,100,'test')";
+	mysql_query($query);
+	
+	//echo $text;
+?>
